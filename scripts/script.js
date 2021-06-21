c4a44633266f456279a9ee82c074b755825a8194
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-06-21 17:11:54 +0200

    🍳 Local Storage + passwords list

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,34 @@ const lowerCaseLetter = new RegExp("(?=(.*[a-z]){1,})");
 const upperCaseLetter = new RegExp("(?=.*[A-Z])");
 const specialSign = new RegExp("(?=.*?[#?!@$%^&*-./\\_])");
 
-const savePasswordInLocalStorage = (password) => {
-  localStorage.setItem();
+let passwords = [];
+
+const savePasswordInLocalStorage = () => {
+  localStorage.setItem("passwords", JSON.stringify(passwords));
+};
+
+const initAddPassword = () => {
+  document
+    .getElementById("login-password-btn")
+    .addEventListener("click", () => {
+      if (!passwords.includes(userPasswordEl.value)) {
+        passwords.push(userPasswordEl.value);
+        savePasswordInLocalStorage();
+        userPasswordEl.value.innerHTML = "";
+      } else alert("This password already exists!");
+    });
+};
+
+const readPasswordsFromLocalStorage = () => {
+  passwords = [];
+  const localPasswords = localStorage.getItem("passwords");
+  if (localPasswords) {
+    const passwordsShapes = JSON.parse(localStorage.getItem("passwords"));
+    passwordsShapes.forEach((password) => {
+      passwords.push(password);
+    });
+  }
+  return passwords;
 };
 
 const initCheckPassword = () => {
@@ -47,6 +73,7 @@ const initCheckPassword = () => {
       console.log(percentages);
     } else percentages += 0;
     changeCheckPasswordButton();
+    closePasswordsList();
   });
 };
 
@@ -62,7 +89,53 @@ const changeCheckPasswordButton = () => {
     color.classList.toggle("yellow");
   }
   if (percentages > 70) {
-    color.classList.add("red");
+    color.classList.toggle("red");
   }
 };
+
+const renderPasswordsList = () => {
+  let passwordsFromStorage = readPasswordsFromLocalStorage();
+  console.log(passwordsFromStorage);
+  passwordsFromStorage.forEach((password) => {
+    const item = document.createElement("p");
+    item.classList.add("passwords-list-item");
+    item.innerHTML = password;
+    document.querySelector(".passwords-list").appendChild(item);
+  });
+};
+
+const initRenderPasswordsList = () => {
+  userPasswordEl.addEventListener("click", () => {
+    setTimeout(() => {
+      document.querySelector(".passwords-list").innerHTML = "";
+      document
+        .querySelector(".passwords-list")
+        .classList.toggle("passwords-list--active");
+      renderPasswordsList();
+    }, 1);
+  });
+};
+
+const closePasswordsList = () => {
+  if (
+    document
+      .querySelector(".passwords-list")
+      .classList.contains("passwords-list--active")
+  )
+    document
+      .querySelector(".passwords-list")
+      .classList.remove("passwords-list--active");
+};
+
+const initClosePasswordsList = () => {
+  document
+    .querySelector("body *:not(.passwords-list--active)")
+    .addEventListener("click", () => {
+      closePasswordsList();
+    });
+};
+
 initCheckPassword();
+initAddPassword();
+initRenderPasswordsList();
+initClosePasswordsList();
